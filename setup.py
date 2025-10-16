f8ffce344157919db37723a2b58d5abe3f4013ee
Asad Prodhan <prodhan82@gmail.com>
2025-10-16 13:26:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,30 @@ from setuptools import setup, find_packages
 setup(
     name="blastdbbuilder",
     version="1.0.0",
-    author="Asad Prodhan",
-    author_email="prodhan82@gmail.com",
-    description="Automated genome downloader, concatenator, and BLAST database builder",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     packages=find_packages(),
-    include_package_data=True,
-    package_data={"blastdbbuilder": ["scripts/*", "containers/*"]},
     entry_points={
         "console_scripts": [
             "blastdbbuilder=blastdbbuilder.cli:main",
         ],
     },
+    install_requires=[
+        # Python packages only, no Singularity or BLAST dependencies
+    ],
     python_requires=">=3.8",
-    license="MIT",
+    include_package_data=True,
+    description="Automated genome downloader, concatenator, and BLAST database builder",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/AsadProdhan/blastdbbuilder",
+    author="Asad Prodhan",
+    author_email="prodhan82@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    package_data={
+        "blastdbbuilder": ["scripts/*"],  # include your shell scripts
+    },
 )
